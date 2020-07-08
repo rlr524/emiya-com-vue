80021363094eb8bb5aef4b265e57e1f4aaffccbb
Rob Ranf <robranf@gmail.com>
2020-07-08 09:46:27 -0700

    converting pricing layout to grid

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,6 +6,10 @@
       You mind your business We'll mind your tech
     </p>
     <p
+      class="hidden"
+      aria-hidden="Main section image - MacBook Pro laptop showing css code"
+    ></p>
+    <p
       class="text-xs sm:text-sm md:text-base lg:text-sm xl:text-base w-1/3 sm:w-1/3 md:w-5/12 lg:w-1/3 text-emiyablack ml-8 mt-2 -let"
     >
       You're not in business to write code. Let us build and manage your web and
@@ -22,8 +26,8 @@ export default {
 
 <style scoped>
 .main-top {
-  background: url(../assets/img/Canva-WebApplicationDeveloperDesk.svg) no-repeat
-    center center/cover;
+  background: url("../assets/img/Canva-WebApplicationDeveloperDesk.svg")
+    no-repeat center center/cover;
   height: 25em;
   width: 100%;
   display: flex;
